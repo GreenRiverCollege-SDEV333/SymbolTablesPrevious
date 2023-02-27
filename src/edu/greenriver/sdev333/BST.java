1896c29ee2b93bbcc9d3cca278a71fd524bd37e7
jack <zengzhenhuai8@gmail.com>
2023-02-27 13:05:02 -0800

    changed #1

--- src/edu/greenriver/sdev333/BST.java
+++ src/edu/greenriver/sdev333/BST.java
@@ -7,21 +7,101 @@ package edu.greenriver.sdev333;
  * @param <ValueType>
  */
 public class BST<KeyType extends Comparable<KeyType>, ValueType> implements OrderedSymbolTable<KeyType, ValueType> {
+
+    private Node root;
+
+    private class Node
+    {
+        private KeyType key;
+        private ValueType val;
+        private Node left;
+        private Node right;
+        private int N; // number of nodes in the subtree rooted here
+
+        public Node(KeyType key, ValueType val, int N)
+        {
+            this.key = key;
+            this.val = val;
+            this.N = N;
+        }
+    }
+
+
+
+
     @Override
     public void put(KeyType key, ValueType value) {
 
     }
 
     @Override
-    public ValueType get(KeyType key) {
+/*    public ValueType get(KeyType key) {
+        // someone gives me a key, i want to find the value that goes with that key
+        Node current = root;
+        while(current != null)
+        {
+            int cmp = key.compareTo(current.key);
+                //compareTo returns, neg, zero, pos.
+            if(cmp < 0)
+            {
+                current = current.left
+
+
+            }
+            else if(cmp>0)
+            {
+                current = current.right;
+            }
+            else
+            {
+                return current.val;
+            }
+        }// end of while loop
+
         return null;
+    }*/
+    public ValueType get(KeyType key) {
+       return get(root,key);
     }
 
+    private ValueType get(Node current, KeyType key)
+    {
+        //author uses x instead of current
+        if(current == null)
+        {
+            return null;
+        }
+
+        int cmp = key.compareTo(current.key);
+
+        if(cmp <0 )
+        {
+            return get(current.left,key);
+        }
+        else if(cmp > 0)
+        {
+            return get(current.right,key);
+        }
+        else{
+            return current.val;
+        }
+    }
     @Override
     public int size() {
-        return 0;
+        return size(root);
     }
 
+    private int size(Node current)
+    {
+        if(current == null)
+        {
+            return 0;
+        }
+        else
+        {
+            return current.N;
+        }
+    }
     @Override
     public KeyType min() {
         return null;
