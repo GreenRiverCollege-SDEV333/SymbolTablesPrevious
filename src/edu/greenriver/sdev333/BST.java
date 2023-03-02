e39e77e863227c9660fd7ee03410bef163173c01
junnynoriega <junnynoriega@gmail.com>
2023-03-02 14:23:44 -0800

    updated files

--- src/edu/greenriver/sdev333/BST.java
+++ src/edu/greenriver/sdev333/BST.java
@@ -7,19 +7,128 @@ package edu.greenriver.sdev333;
  * @param <ValueType>
  */
 public class BST<KeyType extends Comparable<KeyType>, ValueType> implements OrderedSymbolTable<KeyType, ValueType> {
-    @Override
-    public void put(KeyType key, ValueType value) {
+    // field
+    private Node root;
 
+    private class Node {
+        private KeyType key;
+        private ValueType val;
+        private Node left;
+        private Node right;
+        private int N; // number of nodes in the subtree rooted here
+
+        public Node(KeyType key, ValueType val, int N) {
+            this.key = key;
+            this.val = val;
+            this.N = N;
+        }
     }
 
     @Override
+    public void put(KeyType key, ValueType value) {
+        // starts the recursion
+        root = put(root, key, value);
+    }
+
+    public Node put(Node current, KeyType key, ValueType val) {
+        // current is the root of the subtree we are looking at
+
+        // we are at where we are supposed to be
+        if (current == null) {
+            return new Node(key, val,1);
+        }
+
+        int cmp = key.compareTo(current.key);
+        // cmp will be -1 (negative) if key < current.key
+        // cmp will be 0 (zero) if key == current.key
+        // cmp will be +1 (positive) if key > current.key
+
+        if (cmp < 0) {
+            // go left
+            current.left = put(current.left,key,val);
+        }
+        else if (cmp > 0) {
+            // go right
+            current.right = put(current.right,key,val);
+        }
+        else {
+            // key already exists, replace the data(val)
+            current.val = val;
+        }
+
+        // update the node's N - number of nodes in the subtree
+        current.N = size(current.left) + size(current.right) + 1;
+
+        return current;
+    }
+
+
+//    @Override
+//    public ValueType get(KeyType key) {
+//        // someone gives me a key, I want to find the value that goes with that key
+//        Node current = root;
+//        while (current != null) {
+//            int cmp = key.compareTo(current.key);
+//                // compareTo returns neg, zero, pos
+//
+//            if (cmp < 0) {
+//               current = current.left;
+//            }
+//            else if (cmp > 0) {
+//                current = current.right;
+//            }
+//            else {
+//                return  current.val;
+//            }
+//        } // end of while loop
+//
+//        return null;
+//    }
+
+    @Override
     public ValueType get(KeyType key) {
-        return null;
+        return get(root, key);
+    }
+
+    // recursive version
+    // helper method
+    private ValueType get(Node current, KeyType key) {
+        // author uses x instead of current
+        if(current ==  null) {
+            return null;
+        }
+
+        int cmp = key.compareTo(current.key);
+
+        if (cmp < 0) {
+            return get(current.left, key);
+        }
+        else if (cmp > 0) {
+            return get(current.right, key);
+        }
+        else {
+            return current.val;
+        }
     }
 
     @Override
     public int size() {
-        return 0;
+//        if (root == null) {
+//            return 0;
+//        }
+//        else {
+//            return root.N;
+//        }
+        return size(root);
+    }
+
+    private int size(Node current) {
+        if (current == null) {
+            return 0;
+        }
+        else {
+            return current.N;
+        }
     }
 
     @Override
@@ -54,6 +163,25 @@ public class BST<KeyType extends Comparable<KeyType>, ValueType> implements Orde
 
     @Override
     public Iterable<KeyType> keys() {
-        return null;
+        // create a new empty queue to hold my results
+        Queue<KeyType> queue = new Queue<>();
+
+        // start the recursion, collecting results in the queue
+        inorder(root,queue);
+
+        // when done, return the queue
+        return queue;
+    }
+
+    private void inorder(Node current, Queue<KeyType> q) {
+        if (current == null) {
+            // do nothing - end the method immediately
+            return;
+
+        }
+
+        inorder(current.left, q);
+        q.enqueue(current.key);
+        inorder(current.right,q);
     }
 }
--- src/edu/greenriver/sdev333/SequentialSearchST.java
+++ src/edu/greenriver/sdev333/SequentialSearchST.java
@@ -7,14 +7,44 @@ package edu.greenriver.sdev333;
  * @param <ValueType>
  */
 public class SequentialSearchST <KeyType, ValueType> implements SymbolTable<KeyType, ValueType> {
-    @Override
-    public void put(KeyType key, ValueType value) {
+    // field
+    private Node first;
 
+    private class Node {
+        // linked list node
+        private KeyType key;
+        private ValueType val;
+        private Node next;
+
+        public Node(KeyType key, ValueType val, Node next) {
+            this.key = key;
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+    @Override
+    public void put(KeyType key, ValueType val) {
+        // search for key
+        // update value if found; grow table if new
+        for (Node x = first; x != null; x = x.next) {
+            if (key.equals(x.key)) {
+                x.val = val;
+                return; // search hit: update val
+            }
+        }
+        first = new Node(key, val, first); // search miss: add new Node
     }
 
     @Override
     public ValueType get(KeyType key) {
-        return null;
+        // search for key, return associated value
+        for (Node x = first; x != null; x = x.next) {
+            if (key.equals(x.key)) {
+                return x.val; // search hit
+            }
+        }
+        return null; // search miss
     }
 
     @Override
