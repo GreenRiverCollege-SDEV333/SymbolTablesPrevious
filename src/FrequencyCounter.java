b60d07293022d4501236855bb811620580947518
Zoe F <fortin.zoe@student.greenriver.edu>
2023-03-10 15:12:11 -0800

    implement BST

--- src/FrequencyCounter.java
+++ src/FrequencyCounter.java
@@ -1,4 +1,4 @@
-import edu.greenriver.sdev333.SymbolTable;
+import edu.greenriver.sdev333.*;
 
 import java.io.FileNotFoundException;
 import java.util.Scanner;
@@ -17,7 +17,7 @@ public class FrequencyCounter {
     public static void main(String[] args) {
         System.out.println("Hello world!");
 
-        SymbolTable<String, Integer> st = new TreeMapWrapper<>();
+        SymbolTable<String, Integer> st = new BST<>();
 
         try {
             Scanner input = new Scanner(new File(FILENAME));
